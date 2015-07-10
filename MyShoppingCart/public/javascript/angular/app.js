a171528d9361921d3055d13bcf9a0d092824654b
HardikMunjal <hardik.munjal@polestarllp.com>
2015-07-10 12:54:24 +0530

    Design Add Product Form

--- MyShoppingCart/public/javascript/angular/app.js
+++ MyShoppingCart/public/javascript/angular/app.js
@@ -2,22 +2,17 @@ var app = angular.module('SelectApp', [ ]);
 
 app.controller('selectController', function($scope, $http) {
   $scope.category = ['Men', 'Women', 'Electronics', 'Video'];
-  $scope.subcategory = ['Clothing', 'Footwear', 'Fashion', 'Huge'];
+  
   
   $scope.onSelectChangeResult = "";
-  $scope.onEditChangeResult = "";
-  
-  $scope.onEditChange = function () {
-      $scope.onEditChangeResult = "the value is '" + $scope.editValue + "'";
-    };
-    
+ 
     $scope.onSelectChange = function (id) {
         
         var url = '/getsubcategory/'+id;
         $http({
             url: url, 
             method: "GET"
-         }) .success(function(response) {$scope.names = response[0].sc_name;});
+         }) .success(function(response) {$scope.names = response;});
         
       };
       
